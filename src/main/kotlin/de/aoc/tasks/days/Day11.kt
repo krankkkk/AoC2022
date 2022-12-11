742ab7b032a2176fbeb724ea5d1801ef94219461
Tim Jens Krause <tim.krause@iits-consulting.de>
2022-12-11 19:13:09 +0100

    Day11 done

--- src/main/kotlin/de/aoc/tasks/days/Day11.kt
+++ src/main/kotlin/de/aoc/tasks/days/Day11.kt
@@ -3,27 +3,43 @@ package de.aoc.tasks.days
 import de.aoc.tasks.TimeCapturingTask
 import de.aoc.utils.Operator
 import java.io.InputStream
-import java.util.concurrent.atomic.AtomicInteger
 
-class Day11(override val day: Int = 11) : TimeCapturingTask<Map<Int, Day11.Monkey>, Map<Int, Day11.Monkey>> {
 
-    override fun preparePart1Input(input: InputStream): Map<Int, Monkey> =
+class Day11(override val day: Int = 11) : TimeCapturingTask<List<Day11.Monkey>, List<Day11.Monkey>> {
+
+    companion object {
+        private const val ROUNDS_PART1 = 20
+        private const val ROUNDS_PART2 = 10_000
+
+        private val ID_RX = Regex("^Monkey (\\d):$")
+
+        //regex has no way to capture all results of a group, only last match is saved
+        private val ITEMS_RX = Regex("^\\s+Starting items: (\\d+(?:, )?)+$")
+
+        private val OP_RX = Regex("^\\s+Operation: new = old ([\\+\\*\\-\\/\\%]) (\\d+|old)$")
+
+        private val TEST_RX = Regex("^\\s+Test: divisible by (\\d+)$")
+
+        private val NEXT_RX = Regex("^\\s+If (?:true|false): throw to monkey (\\d+)$")
+    }
+
+    override fun preparePart1Input(input: InputStream): List<Monkey> =
         input.bufferedReader().readText()
             .split("\n\n")
             .map { monkey ->
-                val lines = monkey.split("\n")
+                val lines = monkey.split("\n").iterator()
 
-                val id = lines[0].substringAfter("Monkey ").substringBefore(':').toInt()
-                val items = lines[1].substringAfter(':')
-                    .split(',').map(String::trim)
+                val id = ID_RX.find(lines.next())!!.destructured.component1().toInt()
+                val items = lines.next().substringAfter(':')
+                    .split(",")
+                    .map(String::trim)
                     .map(String::toLong)
                     .toMutableList()
 
-                val secondPartOp = lines[2].substringAfter("= old ")
-                val op = Operator.fromOperator(secondPartOp[0])
-                val secondPart = secondPartOp.substring(2).trim()
-                val isSecondPartOld = secondPart == "old"
-                val secondNumber = if (isSecondPartOld) -1L else secondPart.toLong()
+                val opRes = OP_RX.find(lines.next())!!.destructured
+                val op = Operator.fromOperator(opRes.component1().first())
+                val isSecondPartOld = opRes.component2() == "old"
+                val secondNumber = if (isSecondPartOld) -1L else opRes.component2().toLong()
 
                 val operation: (Long) -> Long = { old ->
                     if (isSecondPartOld)
@@ -32,65 +48,57 @@ class Day11(override val day: Int = 11) : TimeCapturingTask<Map<Int, Day11.Monke
                         op.operation(old, secondNumber)
                 }
 
-                val divisible = lines[3].substringAfter("by ").toLong()
+                val divisible = TEST_RX.find(lines.next())!!.destructured.component1().toLong()
 
-                val trueMonkeyID = lines[4].substringAfter("monkey ").toInt()
-                val falseMonkeyID = lines[5].substringAfter("monkey ").toInt()
+                val trueMonkeyID = NEXT_RX.find(lines.next())!!.destructured.component1().toInt()
+                val falseMonkeyID = NEXT_RX.find(lines.next())!!.destructured.component1().toInt()
 
                 Monkey(id, items, divisible, operation, trueMonkeyID, falseMonkeyID)
-            }.associateBy(Monkey::id)
+            }.sortedBy(Monkey::id)
 
-    override fun preparePart2Input(input: InputStream): Map<Int, Monkey> =
+    override fun preparePart2Input(input: InputStream): List<Monkey> =
         preparePart1Input(input)
 
-    override fun executePart1(input: Map<Int, Monkey>): Long =
-        throwItems(input, 20) { it / 3 }
-            .values
-            .map { it.get().toLong() }
+    override fun executePart1(input: List<Monkey>): Long =
+        throwItems(input, ROUNDS_PART1) { it / 3 }
             .sortedDescending()
             .take(2)
             .reduce { acc, l -> acc * l }
 
-    override fun executePart2(input: Map<Int, Monkey>): Long {
-        val mod = input.values
+    override fun executePart2(input: List<Monkey>): Long {
+        val mod = input
             .map(Monkey::testValue)
             .reduce(Operator.MULTIPLY.operation)
 
-        return throwItems(input, 10_000) { it % mod }
-            .values
-            .map { it.get().toLong() }
+        return throwItems(input, ROUNDS_PART2) { it % mod }
             .sortedDescending()
             .take(2)
             .reduce { acc, l -> acc * l }
     }
 
     private fun throwItems(
-        input: Map<Int, Monkey>,
+        input: List<Monkey>,
         rounds: Int,
         postOp: (Long) -> Long
-    ): Map<Int, AtomicInteger> {
-        val counters = buildMap {
-            input.keys.forEach { key ->
-                put(key, AtomicInteger(0))
-            }
-        }
+    ): List<Long> {
+        val counters = buildList { repeat(input.size) { add(0L) } }.toMutableList()
 
         repeat(rounds) {
             repeat(input.size) { monkeyID ->
-                val monkey = input[monkeyID]!!
-                counters[monkeyID]!!.addAndGet(monkey.items.size)
+                val monkey = input[monkeyID]
                 monkey.items
                     .map(monkey.operation)
                     .map(postOp)
-                    .onEach { item ->
+                    .forEach { item ->
                         val id = if (item % monkey.testValue == 0L)
                             monkey.trueMonkeyID
                         else
                             monkey.falseMonkeyID
 
-                        input[id]!!.items.add(item)
+                        input[id].items.add(item)
                     }
 
+                counters[monkeyID] = counters[monkeyID] + monkey.items.size
                 monkey.items.clear()
             }
         }
